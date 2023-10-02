6251e02f8f8d97418092fa927caeac112ef68a82
Erwan <easyastro@enviephoto.fr>
2023-10-02 15:34:20 +0200

    installer

--- ci/pyinstaller.bat
+++ ci/pyinstaller.bat
@@ -1,3 +1,3 @@
 cd ..
-pyinstaller -i src\\resources\\als_logo.ico -D -n als --windowed  --add-data src\\resources\\qt.conf;. --upx-dir C:\Users\eniquet\dev\upx-4.1.0-win64 --paths src\\  src\\start.py
+pyinstaller -i src\\resources\\als_logo.ico -D -n als --windowed  --add-data "src\\resources\\qt.conf;." --upx-dir C:\Users\eniquet\dev\upx-4.1.0-win64 --paths src\\  src\\start.py
 pyinstaller -i src\\resources\\als_logo.ico -F -n alscontrol --console --upx-dir C:\Users\eniquet\dev\upx-4.1.0-win64 src\\als\\als_remote_client.py
